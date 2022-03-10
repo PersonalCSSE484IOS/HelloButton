bb8c71b98fa1d87fcd5bdf7202330781480d6364
Yujie Zhang <zhangy42@rose-hulman.edu>
2022-03-10 09:18:57 -0500

    Finish the hello button

--- HelloButton/ViewController.swift
+++ HelloButton/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var counter = 0;
+    
+    @IBOutlet weak var counterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateView()
     }
 
-
+    @IBAction func pressedButton(_ sender: Any) {
+        let button: UIButton = sender as! UIButton
+        print(button.tag)
+        if(button.tag == -1){
+            counter -= 1
+        }else if(button.tag==0){
+            counter = 0
+        }else if(button.tag==1){
+            counter += 1
+        }else{
+            print("Somethings wrong!")
+        }
+        updateView()
+    }
+    
+    func updateView(){
+        counterLabel.text = "Counter = \(counter)"
+    }
 }
-
